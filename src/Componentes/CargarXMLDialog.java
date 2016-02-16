5ac064c3a9ff9d02697ad800ef0f3ba5081a3823
Manuel28G <man281192@gmail.com>
2016-02-16 04:33:24 -0430

    Clase Dialogo CargarXML
    con esta clase se podrá usar la ventana emergente para cargar un archivo
    a la aplicación

---
+++ src/Componentes/CargarXMLDialog.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Componentes;
+
+import Controller.Util;
+import java.awt.Component;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Manuel Goncalves L.
+ */
+public class CargarXMLDialog extends javax.swing.JFileChooser {
+    
+    int seleccion;
+    File DirDestino;// directorio donde se copiaran los archivos 
+    File DirRecrea;//Donde se copiaran todos los archivos existentes en recrea
+    File DirOrigen;//Donde reciden los archivos XML de recrea
+    File ArchOrigen;//archivo XML origen
+    File ArchDestino;//Archivo XML destino
+    FileNameExtensionFilter filtro = new FileNameExtensionFilter(".xml","xml");
+    String archivoNombre;
+    String strDestino;
+    
+    public CargarXMLDialog(Component cG){
+       this.setFileSelectionMode( JFileChooser.FILES_ONLY);
+       this.setFileFilter(filtro);
+       seleccion = this.showOpenDialog(cG);
+    }
+    
+ public void Cargar()
+ {
+        ArchDestino=this.getSelectedFile();
+        DirDestino=new File(Util.ARCHIVOS_XML_PATH);
+        DirDestino.mkdir();
+        if(ArchDestino.isFile())
+            DirOrigen=new File(ArchDestino.toString());
+        else
+            return;
+        DirDestino=new File(DirDestino+Util.SEPARADOR_DIRECTORIO+ArchDestino.getName());
+        System.out.println(DirOrigen);
+        System.out.println(DirDestino);
+        CrearXML(DirOrigen,DirDestino.toString());
+                  
+ }
+               
+           
+     
+     
+ 
+private static void CrearXML(File fileOrigen,String rutaArchivo) {
+   try{
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    DocumentBuilder db = dbf.newDocumentBuilder();
+    Document docmt = db.parse(fileOrigen);  
+    Source source = new DOMSource(docmt);
+    Result result = new StreamResult(new java.io.File(rutaArchivo)); 
+    Transformer transformer = TransformerFactory.newInstance().newTransformer();
+    transformer.transform(source, result);
+   }
+   catch(Exception e){
+       System.out.println("Error al crear archivo GuardarXMLDialog:"+e);
+   
+   }
+}
+}
