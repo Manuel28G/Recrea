b16a697672b32b0bacea6ebe1688e6a099da43fb
Manuel28G <man281192@gmail.com>
2016-02-23 05:01:11 -0430

    Nuevos ActionListener

--- src/Componentes/ActionsListener.java
+++ src/Componentes/ActionsListener.java
@@ -6,6 +6,7 @@
 
 package Componentes;
 
+import Contrato.ContratoBotones;
 import Contrato.ContratoGeneral;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,66 +17,156 @@ import java.awt.event.ActionListener;
  */
 public class ActionsListener {
  
+ ///Action Botones
+   /**
+    * 
+    * @param Ej
+    * @return 
+    */ 
+  public static ActionListener ActionOpciones (final ContratoBotones cntGen)  {
+     return new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                cntGen.ActionOpciones(e);
+            }
+        };  
+  }
+   /**
+    * 
+    * @param Ej
+    * @return 
+    */ 
+  public static ActionListener ActionSiguiente (final ContratoBotones cntGen)  {
+     return new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                cntGen.ActionSiguiente(e);
+            }
+        };  
+  }
+   /**
+    * 
+    * @param Ej
+    * @return 
+    */ 
+  public static ActionListener ActionSalir (final ContratoBotones cntGen)  {
+     return new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                cntGen.ActionSalir(e);
+            }
+        };  
+  }
+   /**
+    * 
+    * @param Ej
+    * @return 
+    */ 
+  public static ActionListener ActionSonido (final ContratoBotones cntGen)  {
+     return new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                cntGen.ActionSonido(e);
+            }
+        };  
+  }
+  ///FIN Action Botones
+  
+  ///Action MenuBar
+  
+
+   public static ActionListener ActionCargarXML ()  {
+     return new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                Componentes.ImportarXMLDialog cargarXML=new Componentes.ImportarXMLDialog();
+                cargarXML.Cargar();
+                
+            }
+        };  
+  }  
    
-   /**
-    * 
-    * @param Ej
-    * @return 
-    */ 
-  public static ActionListener ActionOpciones (final ContratoGeneral cntGen)  {
+   public static ActionListener ActionGuardarXML ()  {
      return new ActionListener() 
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                cntGen.actionOpciones(e);
+                Componentes.ExportarXMLDialog guardarXML=new Componentes.ExportarXMLDialog();
+                guardarXML.Guardar();
+                
             }
         };  
-  }
-   /**
-    * 
-    * @param Ej
-    * @return 
-    */ 
-  public static ActionListener ActionSiguiente (final ContratoGeneral cntGen)  {
+  }  
+
+   public static ActionListener ActionAgregarMateria (final ContratoGeneral contGen)  {
      return new ActionListener() 
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                cntGen.actionSiguiente(e);
+                View.AgregarMateria nuevaMat=new View.AgregarMateria(contGen);
+                nuevaMat.setVisible(true);
             }
         };  
-  }
-   /**
-    * 
-    * @param Ej
-    * @return 
-    */ 
-  public static ActionListener ActionSalir (final ContratoGeneral cntGen)  {
+  }  
+    
+   public static ActionListener ActionAgregarLeccion (final ContratoGeneral contGen)  {
      return new ActionListener() 
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                cntGen.actionSalir(e);
+                View.AgregarLeccion nuevaLec=new View.AgregarLeccion();
+                nuevaLec.setVisible(true);
             }
         };  
-  }
-   /**
-    * 
-    * @param Ej
-    * @return 
-    */ 
-  public static ActionListener ActionSonido (final ContratoGeneral cntGen)  {
+  }  
+      
+   public static ActionListener ActionAgregarEjercicio (final ContratoGeneral contGen)  {
      return new ActionListener() 
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                cntGen.actionSonido(e);
+                View.AgregarEjercicio nuevaEjer=new View.AgregarEjercicio();
+                nuevaEjer.setVisible(true);
             }
         };  
-  }
+  }  
+        
+   public static ActionListener ActionAvance ()  {
+     return new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                
+            }
+        };  
+  }  
+  
+   public static ActionListener ActionAyuda ()  {
+     return new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                
+            }
+        };  
+  }  
+  
+  ///FIN MenuBar
 
 }
