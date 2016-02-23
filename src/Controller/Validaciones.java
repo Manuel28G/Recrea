3718273b35aa0835622b04fe971b17e455222f03
Manuel28G <man281192@gmail.com>
2016-02-23 05:05:55 -0430

    Archivo validaciones

---
+++ src/Controller/Validaciones.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controller;
+
+import Componentes.ComboBoxRecrea;
+import Componentes.NumberBoxRecrea;
+import Componentes.TAreaRecrea;
+import Componentes.TextBoxRecrea;
+
+/**
+ *
+ * @author Manuel Goncalves L.
+ */
+public class Validaciones {
+    
+    public Validaciones(){
+        
+    }
+    
+    public void ValidarAgregarEjercicio(ComboBoxRecrea cb1,ComboBoxRecrea cb2,
+            ComboBoxRecrea cb3,TextBoxRecrea tb1,NumberBoxRecrea nb1,TAreaRecrea ta1){
+        
+    }
+    
+    public void ValidarAgregarLeccion(){
+        
+    }
+    
+    public void ValidarAgregarMateria(){
+        
+    }
+}
