ef317a4d21f1f89831f77bfe53d5cddf32cfeb6c
Manuel28G <man281192@gmail.com>
2016-02-24 05:02:02 -0430

    Nuevas validaciones

--- src/Controller/Validaciones.java
+++ src/Controller/Validaciones.java
@@ -10,6 +10,13 @@ import Componentes.ComboBoxRecrea;
 import Componentes.NumberBoxRecrea;
 import Componentes.TAreaRecrea;
 import Componentes.TextBoxRecrea;
+import Componentes.VFRecrea;
+import java.awt.Component;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
 /**
  *
@@ -17,20 +24,66 @@ import Componentes.TextBoxRecrea;
  */
 public class Validaciones {
     
+    public static int maxCaracteres=17;
+    private Pattern pt;
+    private Matcher mt;
+    
     public Validaciones(){
         
     }
-    
-    public void ValidarAgregarEjercicio(ComboBoxRecrea cb1,ComboBoxRecrea cb2,
-            ComboBoxRecrea cb3,TextBoxRecrea tb1,NumberBoxRecrea nb1,TAreaRecrea ta1){
-        
+    public boolean longituMaxTextField(JTextField tf){
+        if(tf.getText().length()>maxCaracteres)
+            return false;
+        return true;
     }
     
+    //Validaciones View Agregar Ejercicio
+    public Boolean ValidarComboBox(ComboBoxRecrea cb){
+        if(cb.GetItemRecrea().getNombre().equals(Util.COMBOBOX_SELECCIONAR))
+            return false;
+        return true;
+    }
+    public boolean ValidarRespuesta(Component cpnt){
+       switch(cpnt.getClass().getSimpleName()){
+           case "TextBoxRecrea": return this.ValidarTextField((TextBoxRecrea)cpnt);
+           case "NumberBoxRecrea":return this.ValidarTextField((NumberBoxRecrea)cpnt);
+           case "VFRecrea":return this.ValidarVFRecrea((VFRecrea)cpnt);
+       }
+       return false;
+    }
+    public Boolean ValidarTextField(JTextField tb){
+        
+        pt=Pattern.compile(Util.EXPR_ENTEROyTEXTO);
+        mt=pt.matcher(tb.getText());
+        if(tb.getText().length()==0 || !mt.find())
+            return false;
+        return true;
+    }
+    public Boolean ValidarTextArea(TAreaRecrea ta){
+        pt=Pattern.compile(Util.EXPR_ENTEROyTEXTO);
+        mt=pt.matcher(ta.getText());
+        if(ta.getText().length()==0 || !mt.find())
+            return false;
+        return true;
+    }
+    
+    public Boolean ValidarVFRecrea(VFRecrea vfR){
+        if(vfR.rbFalso.isSelected()|| vfR.rbVerdad.isSelected())
+            return true;
+        return false;
+    }
+    //Validaciones View Agregar Leccion
     public void ValidarAgregarLeccion(){
         
     }
-    
+    //Validaciones View Agregar Materia
     public void ValidarAgregarMateria(){
         
     }
+    
+    //Validaciones view Inicio
+    
+    //Validaciones view Modulos
+    
+    //Validaciones view Practica
 }
