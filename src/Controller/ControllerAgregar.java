a457ba999f52f10a5c9d67ec9961564b6485e12b
Manuel28G <man281192@gmail.com>
2016-03-15 01:19:40 -0430

    retorno en metodos
    se agrego un retorno a los metodos para que puedan devolver el objeto
    creado en cada caso, sea Materia, Leccion o ejercicio

--- src/Controller/ControllerAgregar.java
+++ src/Controller/ControllerAgregar.java
@@ -2,6 +2,9 @@
 package Controller;
 
 import Model.Objetos.Actividad;
+import Model.Objetos.Ejercicio;
+import Model.Objetos.Leccion;
+import Model.Objetos.Materia;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,25 +25,41 @@ public class ControllerAgregar {
      * @param img imagen que se desea asociar a la matéria
      * @param nivl nivel de dificultad de la matéria
      * @param materia nombre de la matéria que se desea crear
+     * @return Materia agregada
      */
-    public void AgregarMateria(String img,String nivl,String materia){
+    public Materia AgregarMateria(String img,String nivl,String materia){
+        Materia respuesta=new Materia();
         String xml= materia.replace(" ", "");
         int maxLong=xml.length();
         if(maxLong>14)
             maxLong=15;
         xml=xml.substring(0,maxLong);
         Model.AgregarXML.XMLMateriasAdd(img,nivl, xml, materia);
+        
+        respuesta.setImagenURL(img);
+        respuesta.setHijoURL(xml+Util.ARCHIVO_XML);
+        respuesta.setNivel(Integer.parseInt(nivl));
+        respuesta.setNombre(materia);
+        return respuesta;
     }
     /**
      * Método para agregar una lección al sistema
      * @param nivel nivel de dificultad de la lección
      * @param imagen imagen a desplegar en pantalla para la lección
      * @param leccion nombre de la lección
-     * @param xmlFile ruta del documento de la matéria previemente creado donde se alojarán las lecciones
+     * @param materiaObj Materia que contiene la ruta del documento previemente creado donde se alojarán las lecciones
+     * @return Leccion creada
      */
-    public void AgregarLeccion(String nivel,String imagen,String leccion,String xmlFile){
-        ControllerConsultar cc= new ControllerConsultar();
+    public Leccion AgregarLeccion(String nivel,String imagen,String leccion,Materia materiaObj){
+  
+        Leccion leccionObj=new Leccion();
+        String xmlFile=materiaObj.getHijoURL();
         Model.AgregarXML.XMLLeccionAdd(nivel,imagen,leccion,xmlFile);
+        leccionObj.setImagenURL(imagen);
+        leccionObj.setNivel(Integer.parseInt(nivel));
+        leccionObj.setNombre(imagen);
+        return leccionObj;
+        
     }
     
     /**
@@ -49,11 +68,20 @@ public class ControllerAgregar {
      * @param puntos puntos del ejercicio
      * @param pregunta pregunta que se realizará o contenido que se desplegará
      * @param Respuesta respuesta correcta para el ejercicio
-     * @param xmlFile ruta del documento de la matéria previemente creado donde se alojarán los ejercicios
-     * @param Leccion nombre de la lección donde se añadirá los ejercicios
+     * @param materiaObj matéria previemente creado donde se alojarán los ejercicios
+     * @param leccionObj lección donde se añadirá los ejercicios
+     * @return Ejercicio creado
      */
-    public void AgregarEjercicio(String tipo,String puntos,String pregunta,String Respuesta,String xmlFile,String Leccion){
+    public Ejercicio AgregarEjercicio(String tipo,String puntos,String pregunta,String Respuesta,Materia materiaObj,Leccion leccionObj){
+       Ejercicio ejercicioObj=new Ejercicio();
+       String xmlFile=materiaObj.getHijoURL();
+       String Leccion=leccionObj.getNombre();
        Model.AgregarXML.XMLEjercicioAdd(tipo,puntos,pregunta,Respuesta,xmlFile,Leccion);
+       ejercicioObj.setEjercicio(pregunta);
+       ejercicioObj.setPuntos(Integer.parseInt(puntos));
+       ejercicioObj.setRespuesta(Respuesta);
+       ejercicioObj.setTipo(tipo);
+       return ejercicioObj;
     }
     
     /**
